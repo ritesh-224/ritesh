a1e24708ab745d362a4f84066ec2f4248a8e82d1
ritesh-224 <riteshvishwakarma224@gmail.com>
2025-08-18 14:40:41 +0530

    adding storage account.

--- rg.tf
+++ rg.tf
@@ -13,6 +13,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "East US"
+  name     = "my-rg"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "stg" {
+  name = "my-stg"
+  location = azurerm_resource_group.example.location
+  resource_group_name" = azurerm_resource_group.example.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
 }
