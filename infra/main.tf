78f740d5cf81b0b441f06ddf63f71195cdf91ac9
ritesh-224 <riteshvishwakarma224@gmail.com>
2025-08-29 12:56:42 +0530

    infra folder added.

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.35.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "bcf304d8-21b8-4358-9c77-54b5dd26f938"
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "my-rg"
   location = "eastus"
