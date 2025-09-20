a02cb82afaf14c46d2ba5417b6e562de0ee06e78
ritesh-224 <riteshvishwakarma224@gmail.com>
2025-09-20 23:46:17 +0530

    terraform required provider added.

--- Virtual_machine/Infra/provider.tf
+++ Virtual_machine/Infra/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.45.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "ecf1c36f-8eba-469e-841f-a595c299e914"
