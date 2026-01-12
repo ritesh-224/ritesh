eeaf8eb0b80d092fb9b8d810cd2abc0d7e8e99e7
ritesh-224 <riteshvishwakarma224@gmail.com>
2026-01-12 13:26:57 +0530

    optimized the modules

--- todoapp_iac/modules/azurerm_resource_group/output.tf
+++ todoapp_iac/modules/azurerm_resource_group/output.tf
@@ -1,4 +1,7 @@
-output "rg_data_block" {
-    description = "The data block for the resource group"
-    value       = azurerm_resource_group.devrg["rg1"].name
+output "rg_names" {
+    description = "Map of resource group names"
+    value = {
+    for k, rg in azurerm_resource_group.devrg :
+    k => rg.name
+  }
 }
--- todoapp_iac/modules/azurerm_sql_database/data.tf
+++ todoapp_iac/modules/azurerm_sql_database/data.tf
@@ -1,8 +1,8 @@
-data "azurerm_resource_group" "data_resource" {
+data "azurerm_resource_group" "rg_names" {
   name = var.rg_name
 }
 
 data "azurerm_mssql_server" "sql_server_info" {
   name                = var.sql_server_name
-  resource_group_name = data.azurerm_resource_group.data_resource.name
+  resource_group_name = data.azurerm_resource_group.rg_names.name
 }
